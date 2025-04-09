819c834720a5aa5f26023230500ac321c02fcf15
Andrey Voronov <avoronov@langate.com>
2025-04-09 23:06:10 +0300

    second version

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -19,14 +19,16 @@ export class SecondComponent implements OnInit {
   rowsVector: number[] = [];
   colsVector: number[] = [];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      inputVectors: this.fb.array(['', '']),
+      inputVectors: this.fb.array([
+        this.fb.group({ vector: '', operation: 'and' }),
+        this.fb.group({ vector: '', operation: 'and' })
+      ]),
       signatureVector: this.fb.control('', Validators.required),
       mergedInputs: this.fb.control(''),
-      operation: this.fb.control('and'),
     });
   }
 
@@ -34,62 +36,95 @@ export class SecondComponent implements OnInit {
     return this.form.controls["inputVectors"] as FormArray;
   }
 
+  get operations() {
+    return this.form.controls["operations"] as FormArray;
+  }
+
   get signatureVector() {
     return this.form.controls["signatureVector"].value;
   }
 
-
   generateResult(): void {
-    const inputVectors: string[] = this.form.get('inputVectors').value;
+    const inputVectors: any[] = this.form.get('inputVectors').value;
     let combinedVector;
-    inputVectors.forEach((vector: string) => {
-      if (!this.isValidInput(vector)) {
+    inputVectors.forEach((control: any) => {
+      if (!this.isValidInput(control.vector)) {
         alert('Enter only 0 or 1');
         return;
       }
     })
 
-    // const mergedIndexes = this.mergedInputs
-    //     .split(',')
-    //     .map((i) => parseInt(i.trim(), 10))
-    //     .filter((i) => !isNaN(i))
+    const mergedIndexes: number[] = this.form.get('mergedInputs').value
+      .split(',')
+      .map((i: string) => parseInt(i.trim(), 10))
+      .filter((i: number) => !isNaN(i))
 
     const signatureArr = this.form.get('signatureVector').value.split('').map(Number);
-    if (inputVectors.length > 2) {
-      combinedVector = inputVectors.reduce((acc, vector, index) => {
-        console.log(acc, vector, index);
-        if (index < inputVectors.length - 1) {
-          const aInt = parseInt(acc, 2);
-          const bInt = parseInt(vector, 2);
-          if (this.form.get('operation').value === 'and') {
-            return (aInt & bInt).toString(2).padStart(acc.length, '0');
-          } else {
-            return (aInt | bInt).toString(2).padStart(acc.length, '0');
+    // if (inputVectors.length > 2) {
+    inputVectors.reduce((acc, vector, reduceIndex) => {
+      console.log(acc, vector, reduceIndex);
+      // if (index < inputVectors.length - 1) {
+      //   const aInt = parseInt(acc, 2);
+      //   const bInt = parseInt(vector, 2);
+      //   if (this.form.get('operation').value === 'and') {
+      //     return (aInt & bInt).toString(2).padStart(acc.length, '0');
+      //   } else {
+      //     return (aInt | bInt).toString(2).padStart(acc.length, '0');
+      //   }
+      // }
+      const rowsVector: number[] = acc.vector?.split('').map(Number);
+      const colsVector: number[] = vector.vector?.split('').map(Number);
+
+      const colHeaders = this.generateBitwiseValues(rowsVector.length);
+      const rowHeaders = this.generateBitwiseValues(colsVector.length);
+      console.log(rowsVector, colsVector)
+      const resultMatrix = rowsVector.map((row, i) =>
+        colsVector.map((col, j) => {
+          const combinedBinaryValue = rowHeaders[i] + colHeaders[j];
+          const noNeedValue = new Set(mergedIndexes.map(i => combinedBinaryValue[i - 1])).size > 1;
+          if (noNeedValue) {
+            return '-';
           }
-        }
-        return acc;
-      })
-    }
+          const index = (row << 1) | col;
+          const signature = reduceIndex < inputVectors.length - 1 ? acc.operation === 'and' ? [0, 0, 0, 1] : [0, 1, 1, 1] : signatureArr;
+          return signature[index] !== undefined ? signature[index] : 0;
+        })
+      );
 
-    this.rowsVector = combinedVector?.split('').map(Number) || inputVectors[0].split('').map(Number);
-    this.colsVector = inputVectors[inputVectors.length - 1].split('').map(Number);
+      const resultString = resultMatrix.flat().filter(i => i !== '-').join('');
+      if (reduceIndex = inputVectors.length - 1) {
+        this.rowsVector = rowsVector;
+        this.colsVector = colsVector;
 
-    this.colHeaders = this.generateBitwiseValues(this.rowsVector.length);
-    this.rowHeaders = this.generateBitwiseValues(this.colsVector.length);
+        this.colHeaders = colHeaders;
+        this.rowHeaders = rowHeaders;
 
-    this.resultMatrix = this.rowsVector.map((row, i) =>
-      this.colsVector.map((col, j) => {
-        // const combinedBinaryValue = this.rowHeaders[i] + this.colHeaders[j];
-        // const noNeedValue = new Set(mergedIndexes.map(i => combinedBinaryValue[i - 1])).size > 1;
-        // if (noNeedValue) {
-        //     return '-';
-        // }
-        const index = (row << 1) | col;
-        return signatureArr[index] !== undefined ? signatureArr[index] : 0;
-      })
-    );
+        this.resultMatrix = resultMatrix;
+        this.resultString = resultString;
+      }
+      return { vector: resultString, operation: vector.operation };
+    })
+    // }
 
-    this.resultString = this.resultMatrix.flat().filter(i => i !== '-').join('');
+    // this.rowsVector = combinedVector?.split('').map(Number) || inputVectors[0].split('').map(Number);
+    // this.colsVector = inputVectors[inputVectors.length - 1].split('').map(Number);
+
+    // this.colHeaders = this.generateBitwiseValues(this.rowsVector.length);
+    // this.rowHeaders = this.generateBitwiseValues(this.colsVector.length);
+
+    // this.resultMatrix = this.rowsVector.map((row, i) =>
+    //   this.colsVector.map((col, j) => {
+    //     const combinedBinaryValue = this.rowHeaders[i] + this.colHeaders[j];
+    //     const noNeedValue = new Set(mergedIndexes.map(i => combinedBinaryValue[i - 1])).size > 1;
+    //     if (noNeedValue) {
+    //         return '-';
+    //     }
+    //     const index = (row << 1) | col;
+    //     return signatureArr[index] !== undefined ? signatureArr[index] : 0;
+    //   })
+    // );
+
+    // this.resultString = this.resultMatrix.flat().filter(i => i !== '-').join('');
   }
 
   isValidInput(vector: string): boolean {
@@ -97,7 +132,8 @@ export class SecondComponent implements OnInit {
   }
 
   addVector(): void {
-    this.inputVectors.push(this.fb.control(''))
+    this.inputVectors.push(this.fb.group({ vector: '', operation: 'and' }));
+    this.cdr.detectChanges();
   }
 
   generateBitwiseValues(n: number) {
